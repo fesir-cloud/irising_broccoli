c56bc399c2f3ccde5eaef9199fb077fe496ce41f
fesir-cloud <55262878+fesir-cloud@users.noreply.github.com>
2023-08-05 23:16:20 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+
+
+
+
+void main {
+#load initial image(w x h Pixels)
+#spread image pixelated over matrix (w x h) <- array
+
+for(int n, n < w, n++){
+#output pixels HEX Values to Perceptrons
+  for(int m, m < h, n++){
+    }
+  }
+
+  
+}
