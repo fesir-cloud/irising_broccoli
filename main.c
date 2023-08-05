4c6d75b2545ca74c5a2e72cbfc072a9dbb67b268
fesir-cloud <55262878+fesir-cloud@users.noreply.github.com>
2023-08-06 00:01:12 +0200

    Update main.c

--- main.c
+++ main.c
@@ -6,9 +6,9 @@ void main {
 //load initial image(w x h Pixels)
 //spread image pixelated over matrix (w x h) <- array
 
-for(int n, n < w, n++){
+for(int n=1, n < w, n++){
 
-  for(int m, m < h, n++){
+  for(int m=1, m < h, n++){
   //Define Perceptron (n|m)
   //output pixel (n|m) HEX Value to Perceptron (n|m) as Parameter
     }
@@ -17,12 +17,17 @@ for(int n, n < w, n++){
   
 }
 
-class node(int hexR; int hexG;int hexB;) {
-//int a=f(n,m,i,1)
-//int a=f(n,m,i,1)
+//class node(int n, int m, int l, int hexR; int hexG;int hexB;) {                    //node soll noch indizes n m und l bekommen
+//int Weighta=mind(n,m,l,1)
+//int Weightb=mind(n,m,l,2)
+//int Weightc=mind(n,m,l,3)
   //int hexR;
   //int hexG;
   //int hexB;
-//Verarbeitung  
+public:
+;
+
+//Verarbeitung aller HexX mit des Weights...und Output des Ergebnittes an node(n,m,l+1)
+
 }
 
